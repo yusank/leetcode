3856df2d29730d54838390b0fec0f6ac521007b5
yusank <yusankurban@gmail.com>
2021-04-19 21:21:32 +0800

    fix: k list reverse

--- all/25.k-个一组翻转链表.go
+++ all/25.k-个一组翻转链表.go
@@ -17,10 +17,61 @@ func reverseKGroup(head *ListNode, k int) *ListNode {
 		return head
 	}
 
+	// 思路： prev 指向翻转的节点的前一个节点
+	// 	翻转开始的节点为slow，终点为fast
+	//  next 为fast的下一个节点，截断 slow之前和fast之后的指向
+	// 	翻转 slow-fast 部分, 然后 prev.Next = fast
+	// slow.Next = next
+	// prev = slow
+	// slow,fast = next,next
 	dum := &ListNode{Next: head}
+	prev := dum
+	slow := head
+	fast := head
+	cnt := 0
+	for fast != nil {
+		if cnt < k-1 {
+			cnt++
+			fast = fast.Next
+			continue
+		}
+
+		// cnt == k-1
+		next := fast.Next
+		// 断开关系
+		fast.Next = nil
+		prev.Next = nil
+
+		// 翻转
+		reverse(slow)
+		prev.Next = fast
+		slow.Next = next
+		slow = next
+		fast = next
+		cnt = 0
+	}
+
+	// 剩下的翻转
+	// 剩余大于0
+	if cnt > 1 {
+		next := fast.Next
+		// 断开关系
+		fast.Next = nil
+		prev.Next = nil
+
+		// 翻转
+		reverse(slow)
+		prev.Next = fast
+		slow.Next = next
+		slow = next
+		fast = next
+		cnt = 0
+	}
+
+	return dum.Next
 }
 
-func reverse(head *ListNode) *ListNode {
+func reverse(head *ListNode) {
 	var (
 		prev *ListNode
 		cur  = head
@@ -32,8 +83,6 @@ func reverse(head *ListNode) *ListNode {
 		prev = cur
 		cur = tempNext
 	}
-
-	return prev
 }
 
 // @lc code=end
